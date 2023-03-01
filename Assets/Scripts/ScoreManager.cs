3b68c77ff6b701ef03d093b7c95e52c1e6cc12ef
Martel <W0460435@campus.nscc.ca>
2023-03-01 15:30:52 -0400

    Initial commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    TMPro.TMP_Text ScoreText;
+    int score;
+    int Score;
+
+    void Start()
+    {
+        
+    }
+}
